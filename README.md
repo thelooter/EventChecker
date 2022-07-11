9baa27b2c79ddee4edfa6dd2ea5036a4395248a5
Lena Kolb <lenakolb2204@gmail.com>
2022-07-11 18:49:05 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,20 @@
+
+![Github Issues](https://img.shields.io/github/issues/thelooter/EventChecker)
+![Github Pull Requests](https://img.shields.io/github/issues-pr/thelooter/EventChecker)
+![Github Forks](https://img.shields.io/github/forks/thelooter/EventChecker)
+![Github License](https://img.shields.io/github/license/thelooter/EventChecker)
+![Github Release](https://img.shields.io/github/v/release/thelooter/EventChecker)
+
+# Event Checker
+
+Plugin to help Developers check which events are getting Fired by the Server.
+Events can be disabled in the config file.
+
+
+## Planned Features
+
+- Add a command to disable/enable events
+- Add a command to see all events
+- Add a command to see all events that are enabled
+- Add a command to see all events that are disabled
+- Add the ability to choose between a whitelist and a blacklist
---
+++ src/main/java/de/thelooter/eventchecker/EventChecker.java
@@ -0,0 +1,97 @@
+package de.thelooter.eventchecker;
+
+import io.github.classgraph.ClassGraph;
+import io.github.classgraph.ClassInfo;
+import io.github.classgraph.ClassInfoList;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.event.Event;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.EventExecutor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
+public class EventChecker extends JavaPlugin implements Listener {
+
+  @Override
+  public void onEnable() {
+    ClassInfoList events =
+        new ClassGraph()
+            .enableClassInfo()
+            .scan()
+            .getClassInfo(Event.class.getName())
+            .getSubclasses()
+            .filter(info -> !info.isAbstract());
+
+    File configFile = new File(getDataFolder(), "config.yml");
+    if (!configFile.exists()) {
+      configFile.getParentFile().mkdirs();
+      saveDefaultConfig();
+    }
+
+    FileConfiguration config = new YamlConfiguration();
+
+    try {
+      config.load(configFile);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    List<String> excludedEvents = config.getStringList("excluded-events");
+
+    Listener listener = new Listener() {};
+
+    EventExecutor executor = (ignored, event) -> getLogger().info("Event: " + event.getEventName());
+
+    List<ClassInfo> enabledEvents =
+        events.stream()
+            .filter(
+                info -> {
+                  for (String excludedEvent : excludedEvents) {
+                    if (info.getName()
+                        .substring(info.getName().lastIndexOf('.') + 1)
+                        .equals(excludedEvent)) {
+                      return false;
+                    }
+                  }
+                  return true;
+                })
+            .toList();
+
+    try {
+      for (ClassInfo info : enabledEvents) {
+
+        @SuppressWarnings("unchecked")
+        Class<? extends Event> eventClass = (Class<? extends Event>) Class.forName(info.getName());
+
+        if (Arrays.stream(eventClass.getDeclaredMethods())
+            .anyMatch(
+                method ->
+                    method.getParameterCount() == 0 && method.getName().equals("getHandlers"))) {
+          getServer()
+              .getPluginManager()
+              .registerEvent(eventClass, listener, EventPriority.NORMAL, executor, this);
+        }
+      }
+    } catch (ClassNotFoundException e) {
+
+      e.printStackTrace();
+    }
+
+    String[] eventNames =
+        events.stream()
+            .map(
+                classInfo ->
+                    classInfo.getName().substring(classInfo.getName().lastIndexOf('.') + 1))
+            .toArray(String[]::new);
+
+    getLogger().info("List of Events: " + String.join(", ", eventNames));
+    getLogger().info("Number of Events: " + events.size());
+    getLogger().info("HandlerList Size:" + HandlerList.getHandlerLists().size());
+  }
+}
