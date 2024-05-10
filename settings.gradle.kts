b66ad54e0ea53cc76e9daa491b2137ab9a6c75be
thelooter <evekolb2204@gmail.com>
2024-05-10 22:55:21 +0200

    Move to new Buildscan plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,15 +1,5 @@
 plugins {
-    id("com.gradle.enterprise") version("3.17.3")
+    id("com.gradle.develocity") version "3.17.3"
 }
 
 rootProject.name = "eventchecker"
-
-gradleEnterprise {
-    if (System.getenv("CI") != null) {
-        buildScan {
-            publishAlways()
-            termsOfServiceUrl = "https://gradle.com/terms-of-service"
-            termsOfServiceAgree = "yes"
-        }
-    }
-}
