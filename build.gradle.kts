b15a878918d40d018f705aec148d3095ababa033
thelooter <evekolb2204@gmail.com>
2024-11-13 14:16:51 +0100

    chore(update): Update to 1.21.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("io.github.goooler.shadow") version "8.1.8"
     jacoco
     id("maven-publish")
+    id("org.openrewrite.rewrite") version "6.26.0"
 }
 
 repositories {
@@ -13,7 +14,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.20.6-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.21.3-R0.1-SNAPSHOT")
     compileOnly("org.jetbrains:annotations:26.0.1")
 
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:2.0.21")
@@ -22,10 +23,17 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.2")
-    testImplementation("com.github.seeseemelk:MockBukkit-v1.20:3.93.2")
+    testImplementation("org.mockbukkit.mockbukkit:mockbukkit-v1.21:4.2.0")
     testImplementation("org.hamcrest:hamcrest-library:3.0")
 
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    rewrite("org.mockbukkit.rewrite:openrewrite-recipes:1.0.2")
+}
+
+rewrite {
+    activeRecipe("org.mockbukkit.rewrite.PackageRename")
+    activeRecipe("org.mockbukkit.rewrite.ClassRename")
 }
 
 val jarVersion = "1.5.0"
--- src/test/kotlin/de/thelooter/eventchecker/EventCheckerTest.kt
+++ src/test/kotlin/de/thelooter/eventchecker/EventCheckerTest.kt
@@ -1,7 +1,7 @@
 package de.thelooter.eventchecker
 
-import be.seeseemelk.mockbukkit.MockBukkit
-import be.seeseemelk.mockbukkit.ServerMock
+import org.mockbukkit.mockbukkit.MockBukkit
+import org.mockbukkit.mockbukkit.ServerMock
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
