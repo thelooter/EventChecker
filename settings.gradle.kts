59ed41530e9c15ff3b11e58d18286545c01043aa
thelooter <evekolb2204@gmail.com>
2026-08-15 22:12:46 +0200

    chore(deps): Update Gradle to 9.7 and migrate dependency management with locking (#243)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,12 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
 plugins {
-    id("com.gradle.develocity") version "4.0.1"
+    id("com.gradle.develocity") version "4.5.0"
 }
 
 rootProject.name = "eventchecker"
@@ -8,6 +15,15 @@ develocity {
     server = "https://scans.gradle.com"
     buildScan {
         termsOfUseUrl = "https://gradle.com/terms-of-service"
-        termsOfUseAgree = if (System.getenv("GITHUB_WORKFLOW").isNullOrEmpty()) "yes" else "no"
+        termsOfUseAgree = "yes"
     }
 }
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        mavenCentral()
+        maven("https://repo.papermc.io/repository/maven-public/")
+    }
+}
+
